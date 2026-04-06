0a3c6836188320bd749b7632fa127f7647ef268b
Kavyakotha013 <155290440+Kavyakotha013@users.noreply.github.com>
2026-04-06 23:48:26 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,11 +1,11 @@
--- Violations table
+-- Violations table fix
 ALTER TABLE violations 
 DROP INDEX uq_vehicle,
 ADD UNIQUE KEY uq_vehicle_sensor (vehicle_id, sensor_code),
 MODIFY violation_count INT DEFAULT 1,
 MODIFY violation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
 
--- Challans table
+-- Challans table fix
 ALTER TABLE challans 
 ADD COLUMN count INT DEFAULT 1,
 ADD COLUMN updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
